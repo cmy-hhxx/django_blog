9f0a6a89b2278190f70208843d4ef79f76cbaf5e
cmy <cmyhhxx@gmail.com>
2021-12-19 23:31:07 +0800

    finish markdown rendering

--- articles/urls.py
+++ articles/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'articles'
 
 urlpatterns = [
-    path('articles_list/', views.articles_list, name='articles_list')
+    path('articles_list/', views.articles_list, name='articles_list'),
+    path('article_detail/<int:id>/', views.article_detail, name='article_detail'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ArticlePost
+import markdown
 
 def articles_list(request):
     # take out all the articles
@@ -12,4 +13,20 @@ def articles_list(request):
     # load the templates and return context object
     return render(request, 'articles/list.html', context)
 
+def article_detail(request, id):
+    # take out the resigned article
+    article = ArticlePost.objects.get(id=id)
+
+    # rendering markdown to html
+    article.body = markdown.markdown(article.body,
+            extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite',
+             ])
+
+    # deliver the article to the templates
+    context = {'article': article}
+
+    # load the templates and return context object
+    return render(request, 'articles/detail.html', context)
 
