fee836130d8b3cfcc6be508e4b0527c0147c7671
cmy <cmyhhxx@gmail.com>
2021-12-19 20:12:36 +0800

    finish part of the models

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class ArticlePost(models.Model):
+    # the author of the article, on_delete means the way when the data is deleted
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # the title of the article
+    title = models.CharField(max_length=200)
+
+    # the body of the article
+    body = models.TextField()
+
+    # the time when the article was posted, default means take the current time
+    created_time = models.DateTimeField(default=timezone.now)
+
+    # the update time of the article , auto_now default writes current time
+    updated_time = models.DateTimeField(auto_now=True)
+
+    # internal class , defines the meta data
+    class Meta:
+        # means the posted articles should be ordered by the time they created reverselly
+        ordering = ('-created_time',)
+
+    # recommended method when this object is called ,return a human readable string
+    def __str__(self):
+        return self.title
