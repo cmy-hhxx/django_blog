eede8aa766bee76cf3ac1c3ddf15e14201ae2de1
cmy <cmyhhxx@gmail.com>
2021-12-22 14:15:38 +0800

    finish the register of uer

--- userprofile/forms.py
+++ userprofile/forms.py
@@ -4,3 +4,19 @@ from django.contrib.auth.models import User
 class UserLoginForm(forms.Form):
     username=forms.CharField()
     password=forms.CharField()
+
+class UserRegisterForm(forms.ModelForm):
+    password=forms.CharField()
+    password_confirm=forms.CharField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+    def clean_password_confirm(self):
+        data = self.cleaned_data
+        if data.get('password') == data.get('password_confirm'):
+            return data.get('password')
+        else:
+            raise forms.ValidationError("密码输入不一致，请重新输入")
+
