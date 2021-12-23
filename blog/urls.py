35b254212c3e33a33a15101ba5f93e6d328f0309
cmy <cmyhhxx@gmail.com>
2021-12-23 19:05:23 +0800

    temporarily finish the blog, urging to be beautified

--- blog/urls.py
+++ blog/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from articles.views import articles_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',articles_list, name='home'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('articles/', include('articles.urls', namespace='articles')),
     path('userprofile/', include('userprofile.urls', namespace='userprofile')),
